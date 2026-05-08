079d921ab43b96cd75ac887b3d783ca7f626cae9
Roberto <robertoguillot23@gmail.com>
2026-05-08 13:42:53 -0500

    Fix: Add load_dotenv() call

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -13,6 +13,8 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 import dj_database_url # Añadir esto
+
+load_dotenv() # Añadir esto
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
