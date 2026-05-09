f142090069d3f66e8ca3b542a30e866fd40a9b86
Jeinerp <143730457+Jeinerp@users.noreply.github.com>
2026-05-08 22:00:06 -0500

    Update models.py

--- backend/core/models.py
+++ backend/core/models.py
@@ -2,19 +2,19 @@ from django.conf import settings
 from django.db import models
 
 class Usuario(models.Model):
-    idusuarios = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=45)
-    apellido = models.CharField(max_length=45)
-    username = models.CharField(max_length=45, unique=True)
-    password = models.CharField(max_length=45)
+    idusuarios = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=45)
+    apellido = models.CharField(max_length=45)
+    username = models.CharField(max_length=45, unique=True)
+    password = models.CharField(max_length=45)
 
-    def __str__(self):
-        return f"{self.nombre} {self.apellido} ({self.username})"
+    def __str__(self):
+        return f"{self.nombre} {self.apellido} ({self.username})"
 
-    class Meta:
-        db_table = 'usuario'
-        verbose_name = "Usuario"
-        verbose_name_plural = "01. Usuarios del Sistema"
+    class Meta:
+        db_table = 'usuario'
+        verbose_name = "Usuario"
+        verbose_name_plural = "01. Usuarios del Sistema"
 
 class Rol(models.Model):
     idrol = models.AutoField(primary_key=True)
